0e39e576080d493417d5336adf1c859edd66ddfd
Sillas Vidal <sillasv91@gmail.com>
2021-05-27 21:52:41 -0300

    configure to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,9 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": {
+    "rejectUnauthorized": false
+  },
   "entities": ["./dist/modules/**/infra/typeorm/entities/*.js"],
   "migrations": ["./dist/shared/infra/typeorm/migrations/*.js"],
   "cli": {
