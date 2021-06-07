e8098080b819f5994918baf7f50e8af552a3afa9
Sillas Vidal <sillasv91@gmail.com>
2021-06-07 20:37:42 -0300

    Created unit tests from Specialist module

---
+++ src/modules/addresses/repositories/fakes/FakeAddressesRepository.ts
@@ -0,0 +1,32 @@
+import { v4 as uuid } from 'uuid';
+
+import ICreateAddressDTO from "@modules/addresses/dtos/ICreateAddressDTO";
+import Address from "@modules/addresses/infra/typeorm/entities/Address";
+import IAddressesRepository from "../IAddressesRepository";
+
+class FakeAddressesRepository implements IAddressesRepository {
+    addresses: Address[] = [];
+    
+    public async create(data: ICreateAddressDTO): Promise<Address> {
+        const address = new Address();
+
+        Object.assign(address, { id: uuid() }, data);
+
+        this.addresses.push(address);
+
+        return address;
+    }
+
+    public async save(address: Address): Promise<Address> {
+        throw new Error("Method not implemented.");
+    }
+    
+    public async findById(address_id: string): Promise<Address | undefined> {
+        const address = this.addresses.find(address => address.id === address_id);
+
+        return address;
+    }
+
+}
+
+export default FakeAddressesRepository;
